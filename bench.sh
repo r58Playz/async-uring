5c8999010cf562597c6c4d7afb7c02ae288db1a0
Toshit Chawda <r58playz@gmail.com>
2025-04-07 20:55:55 -0700

    add to bench script

--- bench.sh
+++ bench.sh
@@ -1,5 +1,7 @@
+CPU="$1"
+shift
 EXAMPLE="$1"
 shift
 
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
-taskset -c 0 bash -c "RUSTFLAGS='-C target-cpu=native -C force-frame-pointers' cargo ${COMMAND:-r} -r --example $EXAMPLE $*"
+taskset -c "$CPU" bash -c "RUSTFLAGS='-C target-cpu=native -C force-frame-pointers' cargo ${COMMAND:-r} -r --example $EXAMPLE $*"
